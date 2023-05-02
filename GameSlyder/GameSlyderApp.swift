9a1b72604fc54c02112f9681c523cb3119335c70
Maksim Gurkov <maksim.gurkov@gmail.com>
2023-05-02 12:55:01 +0300

    Initial Commit

---
+++ GameSlyder/GameSlyderApp.swift
@@ -0,0 +1,17 @@
+//
+//  GameSlyderApp.swift
+//  GameSlyder
+//
+//  Created by Максим Гурков on 02.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct GameSlyderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
